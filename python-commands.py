abe3bfb949f01f75cd88629f138595d5bc833962
tandrey <tandrey@vmware.com>
2022-06-23 21:01:47 -0500

    new python updates

---
+++ python-commands.py
@@ -0,0 +1,7 @@
+# create virtual environment
+(base) tandrey@tandrey-a01 devasc % python3 -m venv myvenv
+(base) tandrey@tandrey-a01 devasc % source myvenv/bin/activate
+(myvenv) (base) tandrey@tandrey-a01 devasc % 
+(myvenv) (base) tandrey@tandrey-a01 devasc % 
+(myvenv) (base) tandrey@tandrey-a01 devasc % deactivate
+(base) tandrey@tandrey-a01 devasc % 
